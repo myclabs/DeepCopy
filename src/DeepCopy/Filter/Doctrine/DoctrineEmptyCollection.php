d6cc07fb8981f3e2d5d327247fe7feb6c8bd0a65
prophet777 <johann_27@hotmail.fr>
2014-08-25 14:05:55 +0200

    Add empty collection filter

--- src/DeepCopy/Filter/Doctrine/DoctrineEmptyCollection.php
+++ src/DeepCopy/Filter/Doctrine/DoctrineEmptyCollection.php
@@ -1,11 +1,4 @@
 <?php
-/**
-* This file is part of the PhpStorm.
-* (c) johann (johann_27@hotmail.fr)
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-**/
 
 namespace DeepCopy\Filter\Doctrine;
 
