2648d9d74581c797c3169ab9c8ee4b285a98f4ac
Kudrin Sergey <s.kudrin@rambler-co.ru>
2019-02-20 22:30:23 +0300

    Cleanup after code review
    - Remove unnecessary phpdoc
    - Switch leading backslash array_unshift to use function
    - Fix for 2.x version
    - Add unit test

--- src/DeepCopy/DeepCopy.php
+++ src/DeepCopy/DeepCopy.php
@@ -17,6 +17,7 @@ use DeepCopy\TypeMatcher\TypeMatcher;
 use ReflectionObject;
 use ReflectionProperty;
 use SplDoublyLinkedList;
+use function array_unshift;
 use function is_array;
 use function is_object;
 use function is_resource;
@@ -85,31 +86,14 @@ final class DeepCopy
         return $this->recursiveCopy($value);
     }
 
-    /**
-     * @param Filter  $filter
-     * @param Matcher $matcher
-     *
-     * @return void
-     */
     public function addFilter(Filter $filter, Matcher $matcher): void
     {
         $this->filters[] = [$matcher, $filter];
     }
 
-    /**
-     * Add filter with high priority
-     *
-     * @param Filter  $filter
-     * @param Matcher $matcher
-     *
-     * @return void
-     */
     public function prependFilter(Filter $filter, Matcher $matcher): void
     {
-        \array_unshift($this->filters, [
-            'matcher' => $matcher,
-            'filter'  => $filter,
-        ]);
+        array_unshift($this->filters, [$matcher, $filter]);
     }
 
     public function addTypeFilter(TypeFilter $filter, TypeMatcher $matcher): void
